a00b55a2e0f276cf356df417b32591693924fad9
Ronnie Tran <ronnie.tran2@gmail.com>
2020-02-26 11:46:47 -0500

    Add tests for experience_item.rs

--- src/experience_item.rs
+++ src/experience_item.rs
@@ -2,27 +2,38 @@ use crate::skill;
 use crate::skill::Skill;
 use std::fmt::Error;
 
-// TODO other experience items?
+// TODO other experience items such as book of knowledge
 
-// TODO return some sort of Option or Result
-// TODO this is ugly lol
-pub fn calculate_number_of_lamps(starting: &Skill, target: &Skill) -> Option<u16> {
-    let mut xp: u32 = starting.xp;
-    let target_xp = target.xp;
+pub fn calculate_number_of_lamps(starting: &Skill, target: &Skill) -> Option<u32> {
+    let mut starting = starting.clone();
     let mut count = 0;
-    loop {
-        if xp >= target_xp {
-            break;
-        }
-        let level = skill::get_xp_to_level(xp)? as u32;
-        let modifier: u32 = (level * 10);
-        xp += modifier;
+    while starting.get_current_xp() < target.get_current_xp() {
+        let gain = (starting.get_current_level() as u32 * 10);
+        starting.gain_xp(gain);
         count += 1;
     }
-
     Some(count)
 }
 
-// TODO tests
+
 #[cfg(test)]
-mod test {}
+mod test {
+    use crate::skill::Skill;
+    use crate::experience_item::calculate_number_of_lamps;
+
+    #[test]
+    fn test_lamps_from_1_to_99() {
+        let starting = Skill::from_level(1).unwrap();
+        let target = Skill::from_level(99).unwrap();
+        let lamps = calculate_number_of_lamps(&starting, &target).unwrap();
+        assert_eq!(lamps, 15_057);
+    }
+
+    #[test]
+    fn test_lamps_from_to_xp() {
+        let starting = Skill::from_xp(12_465).unwrap();
+        let target = Skill::from_xp(32_085).unwrap();
+        let lamps = calculate_number_of_lamps(&starting, &target).unwrap();
+        assert_eq!(lamps, 58);
+    }
+}
