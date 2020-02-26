225de3e7b374a42db88f30e12a47ffd77d0ae147
Ronnie Tran <ronnie.tran2@gmail.com>
2020-02-26 11:48:40 -0500

    Refactor the main file

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,17 @@ use rs_lamps::*;
 fn main() {
     // TODO the CLI
 
-    let starting = rs_lamps::skill::Skill::new(1).expect("Should be fine");
-    let target = rs_lamps::skill::Skill::new(99).expect("Should be fine");
+    let starting = rs_lamps::skill::Skill::from_level(1).expect("Should be fine");
+    let target = rs_lamps::skill::Skill::from_level(99).expect("Should be fine");
 
     let lamps = rs_lamps::experience_item::calculate_number_of_lamps(&starting, &target);
 
-    println!("Starting level: {}, xp: {}", starting.level, starting.xp);
-    println!("Target level: {}, xp: {}", target.level, target.xp);
+
+    println!("Starting level: {}, xp: {}", starting.get_current_level(), starting.get_current_xp());
+    println!("Target level: {}, xp: {}", target.get_current_level(), target.get_current_xp());
     println!(
         "You need to use {} lamps to reach level {}.",
         lamps.unwrap_or_default(),
-        target.level
+        target.get_current_level()
     );
 }
